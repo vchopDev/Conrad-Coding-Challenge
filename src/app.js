12d61427fdb9f652e1d68771ab586a85690e87db
vchopDev <victor.martins@evoluzionetelematica.it>
2020-03-24 13:30:40 +0100

    [Insert your commit message here. Be sure to make it descriptive.] (http://whatthecommit.com)

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const bodyParser = require("body-parser");
-const index = require("./routes/index");
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const index = require("./v1/routes/index");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+app.use(logger('tiny'));
+app.use(bodyParser.json());
+app.use('/', require(path.join(__dirname, './v1/routes')));
+
+
+app.use()
+
 app.use(express.static('public'));
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
-app.use("/", index);
+app.use("/v1/", index);
 
 // app.use("/repositories", repositories);
 // app.use("/bookmarks", bookmarks);
